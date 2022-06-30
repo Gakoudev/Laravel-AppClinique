19f0487bdc27ff7842aa5f5677fdd3f748404044
Gakoudev <tall94@hotmail.fr>
2022-06-30 15:30:24 +0000

    Session
    model & migration

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -31,6 +32,10 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        Session::push('user',[
+            'name'=>Auth::user()->prenom
+        ]);
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
